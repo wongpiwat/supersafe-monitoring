2ba32f644a3889b261190caecf1f46f89db418f8
wongpiwat <wongpiwat.san@gmail.com>
2026-02-25 13:24:51 -0800

    feat: install prettier package and config file

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+};
+
+export default config;
